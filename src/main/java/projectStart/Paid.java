093436a324bdd234b6ae2a817163bfcaa8ad7848
wke <kewoo1996@naver.com>
2020-05-13 22:13:31 +0900

    commit

---
+++ src/main/java/projectStart/Paid.java
@@ -0,0 +1,26 @@
+package projectStart;
+
+public class Paid extends AbstractEvent {
+
+    private Long id;
+    private Long customerId;
+
+    public Paid(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+}
---
+++ src/main/java/projectStart/Payment.java
@@ -0,0 +1,44 @@
+package projectStart;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Payment_table")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private Long customerId;
+
+    @PostPersist
+    public void onPostPersist(){
+        Paid paid = new Paid();
+        BeanUtils.copyProperties(this, paid);
+        paid.publishAfterCommit();
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+
+
+
+}
